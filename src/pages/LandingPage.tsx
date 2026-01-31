d88d7a4f9c0757c1250df822607d3a9b5edfc026
Eduardo <eduardoribeirodefraga05@gmail.com>
2026-01-31 12:19:49 -0300

    feat: integra navbar ao layout principal

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components/landing/Navbar";
+
+export function LandingPage() {
+  return (
+    <div className="min-h-screen bg-background-light font-sans">
+      <Navbar />
+
+     
+    </div>
+  );
+}
