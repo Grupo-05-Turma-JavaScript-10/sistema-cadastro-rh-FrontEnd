a155df19059d320dc14f450d51fd3adad913f146
MariPires96 <mariana.pires96@hotmail.com>
2026-01-31 18:21:43 -0300

    feat: cria componente Footer e configura exibição condicional no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,49 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { Sidebar } from './components/sidebar/Sidebar';
-import { Dashboard } from './pages/Dashboard';
-import { Cargos } from './pages/Cargos';
-import { LandingPage } from './pages/LandingPage';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Sidebar } from "./components/sidebar/Sidebar";
+import { Dashboard } from "./pages/Dashboard";
+import { Cargos } from "./pages/Cargos";
+import { LandingPage } from "./pages/LandingPage";
+import { Footer } from "./components/landing/Footer";
 
 function Layout() {
   const location = useLocation();
-  const isPublicPage = location.pathname === '/';
-
+  const isPublicPage = location.pathname === "/";
 
   return (
-      <div className="flex min-h-screen bg-background-light">
-
+    <div className="flex flex-col min-h-screen bg-background-light">
+      <div className="flex flex-1">
         {!isPublicPage && <Sidebar />}
 
+        <div
+          className={`flex-1 transition-all duration-300 ${
+            !isPublicPage
+              ? "ml-0 md:ml-64 p-4 md:p-8 pt-16 md:pt-8"
+              : "flex flex-col"
+          }`}
+        >
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/cargos" element={<Cargos />} />
 
-       <div className={`flex-1 transition-all duration-300 ${!isPublicPage ? 'ml-0 md:ml-64 p-4 md:p-8 pt-16 md:pt-8' : ''}`}>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/cargos" element={<Cargos />} />
-          
-          <Route path="/colaboradores" element={
-            <div><h1>Em construção...</h1></div>
-          } />
-            
-            <Route path="*" element={<div>Página não encontrada</div>} />
+            <Route
+              path="/colaboradores"
+              element={
+                <div className="p-8 text-corporate-slate italic">
+                  <h1>Em construção...</h1>
+                </div>
+              }
+            />
+
+            <Route
+              path="*"
+              element={<div className="p-8">Página não encontrada</div>}
+            />
           </Routes>
         </div>
-
       </div>
-    
+      {isPublicPage && <Footer />}
+    </div>
   );
 }
 
