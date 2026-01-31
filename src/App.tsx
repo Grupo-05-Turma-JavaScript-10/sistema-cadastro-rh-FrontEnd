21a205555e42c503d0d4ff7212934ac50d02dae2
fernanda <fernandaveeck@gmail.com>
2026-01-31 20:17:59 -0300

    fix: Arquivo Position.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Sidebar } from "./components/sidebar/Sidebar";
 import { Dashboard } from "./pages/Dashboard";
-import { Cargos } from "./pages/Cargos";
+import { Cargos } from "./pages/Position";
 import { LandingPage } from "./pages/LandingPage";
 import { Footer } from "./components/landing/Footer";
 import UsersPage from "./pages/Users";
