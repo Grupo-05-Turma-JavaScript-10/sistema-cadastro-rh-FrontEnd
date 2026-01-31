13ad8b00db9b7a906f3db77edac8894671e5f3f2
Luis Felipe Ramires Leão <luisramires29032002@gmail.com>
2026-01-31 13:02:31 -0300

    feat: cria colaboradorService com endpoints de colaboradores

---
+++ src/service/colaboradorService.ts
@@ -0,0 +1,35 @@
+import type Worker from "../models/Worker";
+import { atualizar, buscar, cadastrar, deletar } from "./baseService";
+
+export const listarColaboradores = async (setDados: Function, header: Object = {}) => {
+    await buscar("/colaboradores", setDados, header);
+};
+
+export const buscarColaboradorPorId = async (id: number, setDados: Function, header: Object = {}) => {
+    await buscar(`/colaboradores/${id}`, setDados, header);
+};
+
+export const buscarColaboradoresPorNome = async (nome: string, setDados: Function, header: Object = {}) => {
+    await buscar(`/colaboradores/nome/${nome}`, setDados, header);
+};
+
+export const criarColaborador = async (colaborador: Worker, setDados: Function, header: Object = {}) => {
+    await cadastrar("/colaboradores", colaborador, setDados, header);
+};
+
+export const atualizarColaborador = async (colaborador: Worker, setDados: Function, header: Object = {}) => {
+    await atualizar("/colaboradores", colaborador, setDados, header);
+};
+
+export const deletarColaborador = async (id: number, header: Object = {}) => {
+    await deletar(`/colaboradores/${id}`, header);
+};
+
+export const calcularSalarioColaborador = async (
+    id: number,
+    payload: Object,
+    setDados: Function,
+    header: Object = {}
+) => {
+    await atualizar(`/colaboradores/calcular-salario/${id}`, payload, setDados, header);
+};
