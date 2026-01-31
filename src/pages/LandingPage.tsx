b2dc097cb53dcdd33eebd7b768db1d5a3f53e67d
MariPires96 <mariana.pires96@hotmail.com>
2026-01-31 20:17:48 -0300

    feat: importa e renderiza o componente Hero na LandingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
+import { Hero } from "../components/landing/Hero";
 import { Navbar } from "../components/landing/Navbar";
 
 export function LandingPage() {
   return (
     <div className="min-h-screen bg-background-light font-sans">
       <Navbar />
-
-     
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 }
