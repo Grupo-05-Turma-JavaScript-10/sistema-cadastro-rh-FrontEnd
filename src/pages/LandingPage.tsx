3f010eb5de467367423d1a6d2a81c57e61da4daf
Eduardo <eduardoribeirodefraga05@gmail.com>
2026-02-01 20:51:39 -0300

    feat: adiciona animacao na Landing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,17 @@
 import { Hero } from "../components/landing/Hero";
 import { Navbar } from "../components/landing/Navbar";
+import { PageTransition } from "../components/ui/PageTransition";
 
 export function LandingPage() {
   return (
+      <PageTransition>
+
     <div className="min-h-screen bg-background-light font-sans">
       <Navbar />
       <main>
         <Hero />
       </main>
     </div>
+    </PageTransition>
   );
 }
